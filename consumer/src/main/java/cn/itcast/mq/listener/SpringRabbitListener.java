4a2da9ccd03e413114e8d01a132213871ca61fa7
ZhouYao0627 <1607435943@qq.com>
2024-01-31 21:30:30 +0800

    Commit 2024_01_31

---
+++ consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
@@ -0,0 +1,13 @@
+package cn.itcast.mq.listener;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringRabbitListener {
+    @RabbitListener(queues = "simple.queue")
+    public void listenSimpleQueueMessage(String msg) {
+        System.out.println("spring 消费者接收到消息：【" + msg + "】");
+    }
+
+}
